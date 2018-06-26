69c797b079a0ad82e618bbccc8ed4a08837be632
Hamadstore <39247812+Hamadstore@users.noreply.github.com>
2018-06-26 19:01:14 +0400

    Update Snapchat.php

--- Pages/Snapchat.php
+++ Pages/Snapchat.php
@@ -1,3 +1,13 @@
+<head>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-1695628203939616",
+          enable_page_level_ads: true
+     });
+</script>
+</head>
+
 <div class="page" data-name="snapchat" data-id="145" data-app-name="Snapchat ++">
 <div class="page-content">
 <center>
