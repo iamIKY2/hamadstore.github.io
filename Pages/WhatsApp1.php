bb2bd5b7d077245ea5c85ec1b8b9a4ccd1905e2e
hugesport <h.dallak@hotmail.com>
2019-02-10 23:47:04 +0400

    Update

--- Pages/WhatsApp1.php
+++ Pages/WhatsApp1.php
@@ -10,7 +10,7 @@
 </div>
 <div class="col-60">
 <h2 class="header" style="padding:0px;"><div id="#appName" style="overflow:hidden;">WhatsApp 1</div><br><div style="margin-top:-35px; font-size:18px;"><small style="color:#6d6d72;">WhatsApp Inc.</small></div></h2>
-<a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/HamadStore/HamadStore.github.io/master/Apps/WhatsApp1/WhatsApp1.plist" class="col button button-fill button-round external install"  data-id="612" data-app-name="WhatsApp1" style="background: #16C333; color: #FFFFFF; font-weight:bold; width: 72px;">تثبيت</a>
+<a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/HamadStore/HamadStore.github.io/master/Apps/WhatsApp/WhatsApp1.plist" class="col button button-fill button-round external install"  data-id="612" data-app-name="WhatsApp1" style="background: #16C333; color: #FFFFFF; font-weight:bold; width: 72px;">تثبيت</a>
 </div>
 </div>
 </div>
--- Pages/WhatsApp4.php
+++ Pages/WhatsApp4.php
@@ -10,7 +10,7 @@
 </div>
 <div class="col-60">
 <h2 class="header" style="padding:0px;"><div id="#appName" style="overflow:hidden;">WhatsApp 4</div><br><div style="margin-top:-35px; font-size:18px;"><small style="color:#6d6d72;">WhatsApp Inc.</small></div></h2>
-<a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/HamadStore/HamadStore.github.io/master/Apps/WhatsApp4/WhatsApp4.plist" class="col button button-fill button-round external install"  data-id="612" data-app-name="WhatsApp4" style="background: #16C333; color: #FFFFFF; font-weight:bold; width: 72px;">تثبيت</a>
+<a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/HamadStore/HamadStore.github.io/master/Apps/WhatsApp/WhatsApp4.plist" class="col button button-fill button-round external install"  data-id="612" data-app-name="WhatsApp4" style="background: #16C333; color: #FFFFFF; font-weight:bold; width: 72px;">تثبيت</a>
 </div>
 </div>
 </div>
